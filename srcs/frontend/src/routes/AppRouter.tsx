d6a829c0e0a8aeda5100ba3f8aa7c59e3fd9dbac
rahmoun03 <rhferminho3@gmail.com>
2025-03-19 02:46:25 +0000

    craeate notification component

--- srcs/frontend/src/routes/AppRouter.tsx
+++ srcs/frontend/src/routes/AppRouter.tsx
@@ -20,8 +20,8 @@ const AppRouter = () => {
 
         <Route path="/login" element={<ProtectedLogin><Login /></ProtectedLogin>} />
         <Route path="/signup" element={<ProtectedLogin><SignUp /></ProtectedLogin>} />
-        <Route path="/verify" element={<ProtectedLogin><Verification/></ProtectedLogin>} />
-        <Route path="/" element={<ProtectedLogin><FirstPage/></ProtectedLogin>} />
+        <Route path="/verify" element={<ProtectedLogin><Verification /></ProtectedLogin>} />
+        <Route path="/" element={<ProtectedLogin><FirstPage /></ProtectedLogin>} />
 
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/news" element={<ProtectedRoute><NewsPage setIsFirstVisible={undefined} /></ProtectedRoute>} /> 
