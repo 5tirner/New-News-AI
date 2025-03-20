1690f2c462b874ab7a0acfb0403173b562007343
C H E B C H O B <115968708+ahbajaou@users.noreply.github.com>
2025-03-20 20:29:04 +0000

    navbar

--- srcs/frontend/src/components/Navbar.tsx
+++ srcs/frontend/src/components/Navbar.tsx
@@ -3,25 +3,36 @@ import  Notification  from "./Notification";
 import { CiLogout } from "react-icons/ci";
 import { useAuth } from "../context/AuthContext";
 
-const Navbar = () => {
+import { useLocation } from "react-router-dom"; // Import useLocation
 
+const Navbar = () => {
   const { logout } = useAuth();
+  const location = useLocation(); // Get current location
+
+  const isHome = location.pathname === "/home";
 
   return (
     <nav className="static top-0 left-0 bg-[#F5E6CF] p-4 border-b border-[#DDBD8A]">
       <div className="container flex justify-between text-white">
         <h1 className="text-xl font-bold text-black">Journalist</h1>
-        <div className="space-x-4">
-          <Link to="/home" className="font-bold text-black">Home</Link>
-        </div>
-        <div className="icons space-x-4 flex">
-          <Notification />
-          <CiLogout 
-            size={30}
-            color="gray"
-            onClick={logout}
-            />
-        </div>
+        
+        {isHome ? (
+          <>
+            <div className="space-x-4">
+              <Link to="/home" className="font-bold text-black">Home</Link>
+            </div>
+            <div className="icons space-x-4 flex">
+              <Notification />
+              <CiLogout 
+                size={30}
+                color="gray"
+                onClick={logout}
+              />
+            </div>
+          </>
+        ) : (
+          null
+        )}
       </div>
     </nav>
   );
